760d00de1cf5063ff9960e2748e51b1a19c3de16
Evgeniy <114279264+dem-ov@users.noreply.github.com>
2023-05-16 10:27:39 +0300

    12

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,15 +1,23 @@
-@import-normalize; /* bring in normalize.css styles */
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+export const GlobalStyle = createGlobalStyle`
 
-body {
-  margin: 0;
+  body {
+   
+
+
+  margin: 80px ;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
+
 
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+  }
+`;
